f6f94aa446ac43f0f66d01d3bfc6092ddc2329fc
Gerald Izuchukwu <geraldlouisugwunna@gmail.com>
2022-10-27 17:53:09 +0100

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8080;
+
+app.use(
+	cors({
+		origin: '*',
+	})
+);
 
 app.get('/', (req, res) => {
 	res.status(200).json({
